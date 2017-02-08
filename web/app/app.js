d188e4500208411afa155f596bd15a68b464e495
Mohammed Khalilia <mohammed.khalilia@gmail.com>
2017-02-08 21:49:42 +0000

    adding example config.js

--- web/app/app.js
+++ web/app/app.js
@@ -31,7 +31,7 @@ define(["ui", "webix"], function(ui) {
     webix.ready(function() {
         ui.init();
 
-        //require(["aperio", "filters"]);
-        require(["aperio", "filters", "pathology", "metadata"]);
+        require(["aperio", "filters"]);
+        //require(["aperio", "filters", "pathology", "metadata"]);
     });
 });
