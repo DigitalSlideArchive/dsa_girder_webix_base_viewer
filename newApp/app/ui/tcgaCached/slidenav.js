8a50f730405e0708f6752509357e5100a2e094e7
DAGLaptop <dagutman@gmail.com>
2018-10-03 17:11:51 -0400

    fixing linking to specific patient

--- newApp/app/ui/tcgaCached/slidenav.js
+++ newApp/app/ui/tcgaCached/slidenav.js
@@ -1,16 +1,33 @@
 define("tcgaCached/slidenav", ["config", "viewer", "pubsub", "slide", "jquery", "tcgaSlideCache", "webix"], function(config, viewer, pubsub, slide, $, tcgaSlideCache) {
-    webix.message("Using Cached DSA Version")
-
-
-    //creating a map of cohortName to ID..
-    cohortNameToId = {};
-    caseNameToId = {};
 
     $.extend({
         getQueryParameters: function(str) {
             return (str || document.location.search).replace(/(^\?)/, '').split("&").map(function(n) { return n = n.split("="), this[n[0]] = n[1], this }.bind({}))[0];
         }
     });
+
+
+        ///look into this..history.pushState(null, "", location.href.split("?")[0]);
+
+        //use this to keep things updated baased on where your navigating.. TO DO FEATURE!
+
+    //This proxy is used to get ALL the items associated with a case using a small subset at a time
+    webix.proxy.PagingGirderItems = {
+        $proxy: true,
+        load: function(view, callback, details) {
+            if (details) {
+                var data = webix.ajax(this.source + "?limit=" + details.count + "&offset=" + details.start);
+            } else {
+                var data = webix.ajax(this.source + "?limit=15&offset=0");
+            }
+
+            data.then(function(resp) {
+                webix.ajax.$callback(view, callback, resp.text());
+            });
+        }
+    };
+    //LAYOUT consists of two dropdowns followed by a dataview
+    //  it's  slideset -->  ptSamples --->slideSelector(dv)
 
     function recomputePagerItems() {
         var itemWidth = $$("slideSelector").config.type.width;
@@ -30,7 +47,7 @@ define("tcgaCached/slidenav", ["config", "viewer", "pubsub", "slide", "jquery", 
         $$("slideSelector").refresh()
     }
 
-    var thumbnailsPanel = {
+    var slideSelector = {
         view: "dataview",
         id: "slideSelector",
         select: true,
@@ -65,79 +82,71 @@ define("tcgaCached/slidenav", ["config", "viewer", "pubsub", "slide", "jquery", 
         group: 4
     };
 
-    //dropdown for slide groups
-    //Data is pulled from DSA webservice
-    //slideset refers to the COHORT/larger group
-
-
-    dropdown = {
+    cohortDropdown = {
         view: "combo",
         placeholder: "Select Cohort",
         id: "slideset",
         options: {
             body: {
-                template: "#name#"
+                template: "#name#",
+                data: tcgaSlideCache.tcgaSlideCache,
             }
         },
         on: {
             onChange: function(id) {
-                var item = this.getPopup().getBody().getItem(id);
-                console.log(id);
+                var cohortItem = this.getPopup().getBody().getItem(id);
                 $$("slideSelector").clearAll();
                 $$("slideSelector").setPage(0);
-                //$$("slideSelector").load(url);
+                //Also clear the thumbnails if any that were being rendered
 
-                //$.get(config.BASE_URL + "/tcga/case?limit=2000&cohort=" + item._id, function(resp) {
-                //   var cases = resp["data"]
-                var ptSamples = $$("ptSamples").getPopup().getList();
-                $$("ptSamples").setValue();
-                ptSamples.clearAll();
-                ptSamples.parse(item["caseListData"]);
+                $$("ptSamples").setValue(); //need to refresh the panel
 
-
-                var url = "PagingGirderItems->" + config.BASE_URL + "/tcga/cohort/" + item._id + "/images";
+                $$("ptSamples").getList().clearAll();
+                $$("ptSamples").getList().parse(cohortItem["caseListData"]);
+                //load the cases for the newly selected cohort
+                var url = "PagingGirderItems->" + config.BASE_URL + "/tcga/cohort/" + cohortItem._id + "/images";
                 $$("slideSelector").clearAll();
                 $$("slideSelector").load(url);
             },
             onAfterRender: webix.once(function() {
 
                 //Migrating to using a cached list...
-
-                // $.get(config.BASE_URL + "/tcga/cohort", function(resp) {
                 var cohorts = tcgaSlideCache.tcgaSlideCache;
-                var cohortList = $$("slideset").getPopup().getList();
-                cohortList.clearAll();
-                cohortList.parse(cohorts);
                 qp = $.getQueryParameters();
-                cohortIdxToLoad = 0; //asssume we load the first value unless the user has requested a valid cohort
+                //FIRST LOAD THE ENTIRE SLIDE CACHE
+                //see if there is a key for this patient in the cohort cache
+                if (tcgaSlideCache.ptIdCache[qp.patientId]) {
+                    selectedCohortId = tcgaSlideCache.ptIdCache[qp.patientId].cohortId;
+                    selectedcaseId = tcgaSlideCache.ptIdCache[qp.patientId].caseId;
 
-                if (qp.patientId) {
-                    console.log(tcgaSlideCache.ptIdCache);  
-                    //the cache converts the patientId to the cohort Id and slide Id
-                    //cohortName = qp.cohort.toLowerCase();
-                    if (tcgaSlideCache.ptIdCache[qp.patientId])
+                    $$("slideset").setValue(selectedCohortId);
+                    $$("ptSamples").getList().clearAll();
+                    $$("ptSamples").getList().parse(
+                        $$("slideset").getList().getItem(selectedCohortId).caseListData
+                    );
 
-                        cohortIdxToLoad = tcgaSlideCache.ptIdCache[qp.patientId].cohortId;
-                    
+                    webix.delay(function() {
+                        $$("ptSamples").setValue(selectedcaseId)
+                    }, "", [], 500);
+
+                } else {
+                    //This loads the slideSet we want
+                    //                console.log("loading cohort",cohortIdxToLoad);
+                    selectedCohortId = $$("slideset").getList().getFirstId();
+                    var ptSamples = $$("ptSamples").getPopup().getList();
+                    console.log(cohorts[0].caseListData);
+                    ptSamples.parse(cohorts[0].caseListData);
+                    $$("slideset").setValue(cohorts[0].id);
+                    //Initially I load all available slides..
+                    var url = "PagingGirderItems->" + config.BASE_URL + "/tcga/cohort/" + selectedCohortId + "/images";
+                    $$("slideSelector").clearAll();
+                    $$("slideSelector").load(url);
                 }
-                //This loads the slideSet we want
-                $$("slideset").setValue(cohortIdxToLoad);
-
-
-                if (qp.patientId) {
-                    //webix.message("Also trying to load a specific patient..")
-                    patientId = qp.patientId;
-            if (tcgaSlideCache.ptIdCache[patientId])
-                    //caseId is the mongoID reference for all the slides associated with the patient
-                $$("ptSamples").setValue(tcgaSlideCache.ptIdCache[qp.patientId].caseId);
-
-                }
-
             })
         }
     };
 
-    samples_dropdown = {
+    ptSamplesDropdown = {
         view: "combo",
         placeholder: "Select Patient",
         id: "ptSamples",
@@ -156,10 +165,10 @@ define("tcgaCached/slidenav", ["config", "viewer", "pubsub", "slide", "jquery", 
                 if (id) {
                     var item = this.getPopup().getBody().getItem(id);
                     var thumbs = $$("slideSelector");
-                    var url = config.BASE_URL + "/tcga/case/" + item._id + "/images";
+                    var BASE_URL = config.BASE_URL + "/tcga/case/" + item.id + "/images";
                     thumbs.clearAll();
                     thumbs.setPage(0);
-                    thumbs.load(url);
+                    thumbs.load(BASE_URL);
                 }
             }
         }
@@ -174,13 +183,13 @@ define("tcgaCached/slidenav", ["config", "viewer", "pubsub", "slide", "jquery", 
         header: "Slides " + wideIcon + narrowIcon,
         onClick: {
             wide: function(event, id) {
-                var count = $$("thumbnails").count();
+                var count = $$("slideSelector").count();
                 this.config.width = 205 * 6;
                 this.resize();
 
                 $$("item_pager").config.size = Math.min(30, count);
                 $$("item_pager").refresh();
-                $$("thumbnails").refresh();
+                $$("slideSelector").refresh();
                 return false;
             },
             narrow: function(event, id) {
@@ -189,13 +198,13 @@ define("tcgaCached/slidenav", ["config", "viewer", "pubsub", "slide", "jquery", 
 
                 $$("item_pager").config.size = 5;
                 $$("item_pager").refresh();
-                $$("thumbnails").refresh();
+                $$("slideSelector").refresh();
                 return false;
             }
         },
         body: {
             rows: [
-                dropdown, samples_dropdown, itemPager, thumbnailsPanel
+                cohortDropdown, ptSamplesDropdown, itemPager, slideSelector
             ]
         },
         width: 220
@@ -204,8 +213,6 @@ define("tcgaCached/slidenav", ["config", "viewer", "pubsub", "slide", "jquery", 
     webix.attachEvent("onResize", function() {
         recomputePagerItems();
     })
-
-
 
     return slidesPanel;
 });
